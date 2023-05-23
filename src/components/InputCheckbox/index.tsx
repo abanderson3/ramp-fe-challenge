afe58a7612b4ced4d83583c503d6fda39356fd09
Bryce Anderson <118213399+abanderson3@users.noreply.github.com>
2023-05-23 18:14:33 +0000

    Have resolved bugs 1-6.

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,13 +1,26 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
+
+  const handleCheckboxChange = () => {
+    const newValue = !checked;
+    // console.log("Checkbox value changed:", newValue);
+    onChange(newValue);
+  };
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
+        // the CSS label was positioned on top of the input, caused the pointer act as expected but blocked any clicks from reaching the input and triggering click events or changes
+        htmlFor={inputId} // Bug #2 resolved through for attribute
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
@@ -19,8 +32,8 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={handleCheckboxChange}
       />
     </div>
-  )
-}
+  );
+};
--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react"
-import { InputCheckbox } from "../InputCheckbox"
-import { TransactionPaneComponent } from "./types"
+import { useState, useEffect } from "react";
+import { InputCheckbox } from "../InputCheckbox";
+import { TransactionPaneComponent } from "./types";
 
 export const TransactionPane: TransactionPaneComponent = ({
   transaction,
   loading,
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-  const [approved, setApproved] = useState(transaction.approved)
+  const [approved, setApproved] = useState(transaction.approved);
+
+  // useEffect(() => {
+  //   setApproved(transaction.approved);
+  // }, [transaction.approved]);
 
   return (
     <div className="RampPane">
@@ -15,7 +19,8 @@ export const TransactionPane: TransactionPaneComponent = ({
         <p className="RampText">{transaction.merchant} </p>
         <b>{moneyFormatter.format(transaction.amount)}</b>
         <p className="RampText--hushed RampText--s">
-          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+          {transaction.employee.firstName} {transaction.employee.lastName} -{" "}
+          {transaction.date}
         </p>
       </div>
       <InputCheckbox
@@ -26,16 +31,16 @@ export const TransactionPane: TransactionPaneComponent = ({
           await consumerSetTransactionApproval({
             transactionId: transaction.id,
             newValue,
-          })
+          });
 
-          setApproved(newValue)
+          setApproved(newValue);
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-})
+});
